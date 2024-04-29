c3ecb1c740246ed3c1c6ef83485db60e50f7837c
DesireeAv <desiree.aviles.a@gmail.com>
2024-04-28 22:22:46 -0600

    Delete DataBaseFiles

--- DataBaseFiles/modify27_04.sql
+++
@@ -1,9 +0,0 @@
-alter table PERSON
-add email varchar2(300);
-
-select * from person;
-
-UPDATE person
-SET email = 'user@gmail.com'
-WHERE id in (1,2,3,4,5,21,22);
-
--- DataBaseFiles/selects25_04.sql
+++
@@ -1,7 +0,0 @@
-select * from KINGDOM
-select * from PHYLUM;
-select * from family;
-select * from genus;
-select * from CLASS;
-select * from orden;
-select * from SPECIES;
